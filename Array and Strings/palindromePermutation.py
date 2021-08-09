8cb9016d231500cbff80e9cccd40a14785285fd0
mushahidmehdi <mushahidmehdi14@gmail.com>
2021-08-09 12:12:49 +0300

    second

---
+++ Array and Strings/palindromePermutation.py
@@ -0,0 +1,30 @@
+# Check whether the given string is palindrome or not.
+
+from logging import StringTemplateStyle
+import unittest
+
+def palindrome_pythonic(str):
+	str = str.replace(' ', '')
+	new_str = "".join(reversed(str))
+	if str == new_str:
+		return str
+	
+
+def palindrome(str):
+	str = str.replace(' ', '')
+	if str == str[::-1]:
+		return str
+	return str
+class Test(unittest.TestCase):
+	def test_palindrome_pythonic(self):
+		self.assertEqual(palindrome_pythonic('aba'), 'aba')
+		self.assertEqual(palindrome_pythonic('redivider'), 'redivider')
+		self.assertEqual(palindrome_pythonic('mr owl ate my metal worm'), 'mrowlatemymetalworm' )
+	def test_palindrome(self):
+		self.assertEqual(palindrome('aba'), 'aba')
+		self.assertEqual(palindrome('redivider'), 'redivider')
+		self.assertEqual(palindrome('mr owl ate my metal worm'), 'mrowlatemymetalworm')
+
+
+if __name__ == '__main__':
+	unittest.main()
--- Array and Strings/urlify.py
+++ Array and Strings/urlify.py
@@ -1,12 +1,32 @@
 import unittest
+from unittest import result
 from unittest.case import TestCase
 
 def urlify_pythonic(str, len):
 	return str[:len].replace(' ', '%20')
 
 
+def urlify(str, len):
+	result = ''
+	new_str = str[:len]
+	for i in new_str:
+		if i == ' ':
+			result += "%20"
+		else:
+			result += i 
+		
+	return result
+
+
+
+
 class Test(unittest.TestCase):
 	def test_urlify(self):
+		self.assertEqual(urlify('much ado about nothing      ', 22), "much%20ado%20about%20nothing")
+		self.assertEqual(urlify("Mr John Smith       ", 13), "Mr%20John%20Smith")
+		self.assertEqual(urlify(" a b", 4), "%20a%20b")
+
+	def test_urlify_pythonic(self):
 		self.assertEqual(urlify_pythonic('much ado about nothing      ', 22), "much%20ado%20about%20nothing")
 		self.assertEqual(urlify_pythonic("Mr John Smith       ", 13), "Mr%20John%20Smith")
 		self.assertEqual(urlify_pythonic(" a b", 4), "%20a%20b")
